93d3c8cd89aa2189620ebcb009273276d6375dd3
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-08-01 22:59:36 +0530

    trying

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -1,13 +1,13 @@
 import Router from 'express'
-import { sessionTime, verifyJWT } from '../middlewares/auth.middleware.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 import { editTask, isFinished, newTask, removeTask } from '../controllers/task.controller.js';
 
 
 const taskRouter = Router();
 
-taskRouter.route('/add-new-task').post(sessionTime,verifyJWT,newTask)
-taskRouter.route('/editTask').post(sessionTime,verifyJWT,editTask);
-taskRouter.route('/removeTask').post(sessionTime,verifyJWT,removeTask);
-taskRouter.route('/completed').post(sessionTime,verifyJWT,isFinished);
+taskRouter.route('/add-new-task').post(verifyJWT,newTask)
+taskRouter.route('/editTask').post(verifyJWT,editTask);
+taskRouter.route('/removeTask').post(verifyJWT,removeTask);
+taskRouter.route('/completed').post(verifyJWT,isFinished);
 
 export {taskRouter}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,18 +1,19 @@
 import Router from 'express'
 import { upload } from '../middlewares/multer.js';
 import { allFolders, folderTasks, login, logout, signup, taskList } from '../controllers/user.controller.js';
-import { sessionTime, verifyJWT } from '../middlewares/auth.middleware.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 import { addCategory } from '../controllers/category.controller.js';
 
 const userRoute = Router();
 
 userRoute.route("/signup").post(upload.single("avatar"),signup);
 userRoute.route("/login").post(login);
-userRoute.route("/logout").post(sessionTime,verifyJWT ,logout);
-userRoute.route('/tasks').post(sessionTime,verifyJWT ,taskList)
-userRoute.route('/folder-tasks').post(sessionTime,verifyJWT ,folderTasks)
-userRoute.route('/new-folder').post(sessionTime,verifyJWT ,addCategory)
-userRoute.route('/all-folders').post(sessionTime,verifyJWT ,allFolders)
+userRoute.route("/logout").post(verifyJWT ,logout);
+userRoute.route('/tasks').post(verifyJWT ,taskList)
+userRoute.route('/folder-tasks').post(verifyJWT ,folderTasks)
+userRoute.route('/new-folder').post(verifyJWT ,addCategory)
+userRoute.route('/all-folders').post(verifyJWT ,allFolders)
+
 
 
 export {userRoute}
